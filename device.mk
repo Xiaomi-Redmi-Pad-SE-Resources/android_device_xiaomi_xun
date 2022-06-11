4d10e6d605f03e530b2ce2dc5103503b4081cc1e
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2022-06-11 02:02:20 +0000

    xun: Enable freeform multi-windows

--- device.mk
+++ device.mk
@@ -23,6 +23,10 @@ TARGET_SCREEN_WIDTH := 1920
 PRODUCT_COPY_FILES += \
     $(KERNEL_PATH)/dtb:dtb.img
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # Init scripts
 PRODUCT_PACKAGES += \
     init.device.rc
