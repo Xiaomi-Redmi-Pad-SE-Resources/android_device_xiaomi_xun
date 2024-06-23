56d0cdbcde6ce5f29dfcbf6ef0a177e8d61e60bd
Bruno Martins <bgcngm@gmail.com>
2024-06-23 15:42:45 +0100

    xun: Declare IMS and WFD libs as symlinks during extraction
    Change-Id: I2a035323a483b8b2422b4a54219b2f6268ab4a61

--- Android.mk
+++ Android.mk
@@ -47,23 +47,7 @@ $(EGL_LIB64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	$(hide) ln -sf egl/libGLESv2_adreno.so $@/libGLESv2_adreno.so
 	$(hide) ln -sf egl/libq3dtools_adreno.so $@/libq3dtools_adreno.so
 
-IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
-IMS_SYMLINKS := $(addprefix $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
-$(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "IMS lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /system_ext/lib64/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
-
-WFD_SERVICE_SYMLINKS := $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/WfdService/lib/arm64
-$(WFD_SERVICE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "WFD service lib link: $@"
-	@mkdir -p $@
-	$(hide) ln -sf /system_ext/lib64/libwfdnative.so $@/libwfdnative.so
-
-ALL_DEFAULT_INSTALLED_MODULES += $(EGL_LIB_SYMLINKS) $(EGL_LIB64_SYMLINKS) $(WFD_SERVICE_SYMLINKS)
+ALL_DEFAULT_INSTALLED_MODULES += $(EGL_LIB_SYMLINKS) $(EGL_LIB64_SYMLINKS)
 
 PREBUILT_KERNEL_MODULES_SYSTEM_DLKM := $(TARGET_OUT_SYSTEM_DLKM)/lib/modules
 $(PREBUILT_KERNEL_MODULES_SYSTEM_DLKM):
