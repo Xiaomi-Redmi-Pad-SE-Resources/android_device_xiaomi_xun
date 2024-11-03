040e8d83535fbb71dec3b34c8f51d6007f810f96
Michael Bestas <mkbestas@lineageos.org>
2024-11-03 07:33:51 +0200

    xun: Switch to python extract-utils
    Change-Id: Ida4b83459d9739ac880b26a91df974e1db8423e9

---
+++ extract-files.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/xiaomi/xun',
+    'hardware/qcom-caf/sm6225',
+    'hardware/qcom-caf/wlan',
+    'hardware/xiaomi',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/dataservices',
+]
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'vendor.qti.diaghal@1.0',
+        'vendor.qti.hardware.fm@1.0',
+        'vendor.qti.hardware.dpmservice@1.0',
+        'vendor.qti.hardware.qccsyshal@1.0',
+        'vendor.qti.hardware.qccsyshal@1.1',
+        'vendor.qti.hardware.qccsyshal@1.2',
+        'vendor.qti.hardware.qccvndhal@1.0',
+        'vendor.qti.hardware.wifidisplaysession@1.0',
+        'vendor.qti.imsrtpservice@3.0',
+        'vendor.qti.imsrtpservice@3.1',
+    ): lib_fixup_vendor_suffix,
+    (
+        'libagmclient',
+        'libpalclient',
+        'libwfdaac_vendor',
+        'libwpa_client',
+    ): lib_fixup_remove,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
+    'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .add_needed('libbinder_shim.so')
+        .add_needed('libinput_shim.so'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
+    'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
+        .add_needed('libhidlbase_shim.so'),
+    (
+        'odm/etc/xuncs_build.prop',
+        'odm/etc/xunes_build.prop',
+        'odm/etc/xungs_build.prop',
+        'odm/etc/xunis_build.prop',
+    ): blob_fixup()
+        .regex_replace('cert', 'model'),
+    (
+        'vendor/bin/hw/vendor.qti.hardware.display.composer-service',
+        'vendor/bin/hw/vendor.xiaomi.hardware.displayfeature@1.0-service',
+        'vendor/lib64/hw/vendor.xiaomi.hardware.displayfeature@1.0-impl.so',
+        'vendor/lib64/libdisplayfeatureservice.so',
+        'vendor/lib64/libsdmcore.so',
+    ): blob_fixup()
+        .replace_needed("vendor.xiaomi.hardware.displayfeature@1.0.so", "libvendor.xiaomi.hardware.displayfeature@1.0.so"),
+    (
+        'vendor/bin/hw/android.hardware.security.keymint-service-qti',
+        'vendor/lib64/libqtikeymint.so'
+    ): blob_fixup()
+        .add_needed('android.hardware.security.rkp-V3-ndk.so'),
+    (
+        'vendor/lib/libdlbpreg.so',
+        'vendor/lib/libqc2audio_hwaudiocodec.so',
+        'vendor/lib/soundfx/libdlbvol.so',
+        'vendor/lib/soundfx/libhwdap.so',
+        'vendor/lib64/hw/displayfeature.default.so',
+        'vendor/lib64/libdlbdsservice.so',
+        'vendor/lib64/libdlbpreg.so',
+        'vendor/lib64/libqc2audio_hwaudiocodec.so',
+        'vendor/lib64/soundfx/libdlbvol.so',
+        'vendor/lib64/soundfx/libhwdap.so',
+    ): blob_fixup()
+        .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'xun',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
