d765f3ae38447aab8864aa7f890c30e21c9c3870
chandu078 <chandudyavanapelli03@gmail.com>
2024-12-12 10:52:35 +0530

    xun: Work around to copy the prebuilt kernel system_dlkm modules
    Required after enabling ELF binary checks.
    
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- Android.mk
+++ Android.mk
@@ -8,4 +8,13 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),xun)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+
+PREBUILT_KERNEL_MODULES_SYSTEM_DLKM := $(TARGET_OUT_SYSTEM_DLKM)/lib/modules
+$(PREBUILT_KERNEL_MODULES_SYSTEM_DLKM):
+	@mkdir -p $(TARGET_OUT_SYSTEM_DLKM)/lib/modules
+	@cp -r $(KERNEL_PATH)/modules/system/* $(PREBUILT_KERNEL_MODULES_SYSTEM_DLKM)
+ALL_DEFAULT_INSTALLED_MODULES += $(PREBUILT_KERNEL_MODULES_SYSTEM_DLKM)
+
 endif
