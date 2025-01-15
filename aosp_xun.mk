afad5ec647cfc3a2800642eb6951cd4524dcd080
dodyirawan85 <dev.irawans@gmail.com>
2025-01-16 01:46:41 +0700

    xun: Bringup PixelOS
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- aosp_xun.mk
+++ aosp_xun.mk
@@ -8,13 +8,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_tablet_wifionly.mk)
 
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/xun/device.mk)
 
-PRODUCT_NAME := lineage_xun
+PRODUCT_NAME := aosp_xun
 PRODUCT_DEVICE := xun
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
@@ -25,6 +25,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="xun-user 15 AQ3A.240829.003 OS2.0.2.0.VMUMIXM release-keys" \
     BuildFingerprint=Redmi/xun/xun:15/AQ3A.240829.003/OS2.0.2.0.VMUMIXM:user/release-keys
-
-# Time
-LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
