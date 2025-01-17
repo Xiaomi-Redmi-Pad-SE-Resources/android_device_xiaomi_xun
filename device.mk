2425205479e0854d53ad2e09f91ede2fff668399
dodyirawan85 <dev.irawans@gmail.com>
2025-01-17 17:06:45 +0700

    Revert "xun: Add init script to stop `vendor.hvdcp_opti` after boot"
    * We alredy move this blobs/service into device specific, is should work right ?
    
    This reverts commit 466661bbc72acee073b1f56ecf1c5ea38f908713.

--- device.mk
+++ device.mk
@@ -27,10 +27,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
 
-# Init scripts
-PRODUCT_PACKAGES += \
-    init.device.rc
-
 # Set product characteristic to tablet, needed for some ui elements
 PRODUCT_CHARACTERISTICS := tablet
 
