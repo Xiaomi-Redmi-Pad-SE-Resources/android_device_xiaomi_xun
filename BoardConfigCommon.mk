e8a65f47064a0de163f531ca97fe2d5cbd0aa571
dodyirawan85 <dev.irawans@gmail.com>
2025-02-21 08:57:52 +0000

    Revert "sm6225-common: Drop soong configs for audio service"
    This reverts commit d626f7fc8513d814fcd341f15bfa5f61c521259b.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -39,6 +39,8 @@ TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
 
 # Audio
+$(call soong_config_set, android_hardware_audio, run_64bit, true)
+
 AUDIO_FEATURE_ENABLED_DLKM := true
 AUDIO_FEATURE_ENABLED_DTS_EAGLE := false
 AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT := true
