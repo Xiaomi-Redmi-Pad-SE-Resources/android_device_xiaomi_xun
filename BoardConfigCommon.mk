2ac98e0914a4405526ad277f3ea59e69c0462314
dodyirawan85 <dev.irawans@gmail.com>
2025-06-03 23:05:15 +0700

    sm6225-common: sepolicy: Label `service.sf.present_timestamp` property
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -176,6 +176,7 @@ VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include device/xiaomi/sepolicy/SEPolicy.mk
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
