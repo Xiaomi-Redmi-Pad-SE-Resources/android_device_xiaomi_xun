521fa56e1fdd1a40560c2b335b6c537f6e85c911
dodyirawan85 <dev.irawans@gmail.com>
2025-09-05 01:01:31 +0700

    xun: Ditch common xiaomi sepolicies
    * We're gonna try to commonize sepolicies on hw/ximi like oplus and moto did
    
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -178,7 +178,6 @@ VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
-include device/xiaomi/sepolicy/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
