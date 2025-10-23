135942ebb84e3eca0b18d84e65a6927622d4be5c
LuK1337 <priv.luk@gmail.com>
2025-10-23 23:18:07 +0200

    xun: Set PRODUCT_ENABLE_UFFD_GC to true
    This is not really necessary, but setting it prevents the build system
    from attempting to determine UFFD GC support on its own, during which
    it's triggering unnecessary kernel builds when compiling any app/jar.
    
    Change-Id: I8b8958f31c5437310ea27250447c8139e1684dcc

--- device.mk
+++ device.mk
@@ -201,6 +201,8 @@ PRODUCT_PACKAGES += \
 $(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):init_xiaomi_bengal)
 
 # Kernel
+PRODUCT_ENABLE_UFFD_GC := true
+
 PRODUCT_COPY_FILES += \
 	$(KERNEL_PATH)/kernel:kernel
 
