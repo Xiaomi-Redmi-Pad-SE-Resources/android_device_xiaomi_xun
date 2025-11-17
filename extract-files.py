3b604fe9e09852bba748a6aa935570d788db9c26
dodyirawan85 <dev.irawans@gmail.com>
2025-11-17 23:37:28 +0700

    xun: Cleanup IMS / RIL stack leftover
    * Also reorder while we're at it
    
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -45,8 +45,6 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.hardware.qccsyshal@1.2',
         'vendor.qti.hardware.qccvndhal@1.0',
         'vendor.qti.hardware.wifidisplaysession@1.0',
-        'vendor.qti.imsrtpservice@3.0',
-        'vendor.qti.imsrtpservice@3.1',
     ): lib_fixup_vendor_suffix,
     (
         'libagmclient',
