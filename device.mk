c9009a2713749b25c827ecbbb7480777aa262e72
dodyirawan85 <dev.irawans@gmail.com>
2025-12-05 06:32:30 +0700

    xun: Drop useless hidl bluetooth impl
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- device.mk
+++ device.mk
@@ -96,7 +96,6 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
-    android.hardware.bluetooth.audio@2.1-impl \
     audio.bluetooth.default
 
 PRODUCT_COPY_FILES += \
