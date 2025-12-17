672f9460c242072321022cf1631b741c039f7d40
dodyirawan85 <dev.irawans@gmail.com>
2025-12-17 22:54:37 +0700

    xun: Do not manually built audio libraries
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- device.mk
+++ device.mk
@@ -62,7 +62,6 @@ PRODUCT_PACKAGES += \
     libagm_compress_plugin \
     libagm_mixer_plugin \
     libagm_pcm_plugin \
-    libaudiopreprocessing \
     libbatterylistener \
     libfmpal \
     libhfp_pal \
@@ -70,7 +69,6 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libsndcardparser \
-    libtinycompress \
     libvolumelistener
 
 AUDIO_HAL_DIR := hardware/qcom-caf/sm6225/audio/primary-hal
